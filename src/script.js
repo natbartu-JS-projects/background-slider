1a44ab7ac375dac427e9f21bbac172cc35aacb9a
natacha <nat_bartu@hotmail.com>
2022-08-16 14:32:41 +1000

    get all the elements into the DOM with JS and set background image

---
+++ src/script.js
@@ -0,0 +1,12 @@
+const body = document.body;
+const slides = document.querySelectorAll(".slide");
+const leftBtn = document.getElementById("left");
+const rightBtn = document.getElementById("right");
+
+let activeSlide = 0;
+
+setBackgroundToBody();
+
+function setBackgroundToBody() {
+  body.style.backgroundImage = slides[activeSlide].style.backgroundImage;
+}
